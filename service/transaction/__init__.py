81c00b0096b9bec7eb49d6a4031306f5714a302d
Outsrkem <xhdascnf@126.com>
2021-09-05 19:27:53 +0800

    汇总的统计了总金额，修改字段类型 (ledger_recedisbu_statement)

--- service/transaction/__init__.py
+++ service/transaction/__init__.py
@@ -1,19 +1,31 @@
 # -*- coding=utf-8 -*-
+from decimal import Decimal
+
 
 def statistical_trim(data=list()):
     # data 固定内容如下
     # data = [{"amount_total": "10000005.0000", "money_title": "餐饮", "money_type": 1001, "record_period": "2021-10"}]
     response = dict()
-    row_list = list()
+    rows = list()
+    amount_total = list()
+    amount_sum = Decimal()
     if data:
         details, period, row = list(), '', dict()
         for i in data:
+            amount_total.append(i["amount_total"])
             i["amount_total"] = str(i["amount_total"])
             period = i["record_period"]
             details.append(i)
 
         for i in details:
             row[i["money_type"]] = i["amount_total"]
-        row_list.append(row)
-        response["details"], response["period"], response["rows"] = details, period, row_list
+        rows.append(row)
+
+        for i in amount_total:  # 计算总金额
+            amount_sum = amount_sum + i
+        amount_sum = amount_sum.quantize(Decimal('0.000'))  # 四舍五入，保留3位小数
+
+        response["details"], response["period"], response["rows"], response["amount_sum"] = \
+            details, period, rows, str(amount_sum)
+
     return response
