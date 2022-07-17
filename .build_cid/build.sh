e2eeaf23a25f1b628eae15ba39b67d65febd2b39
Outsrkem <xhdascnf@126.com>
2022-07-17 22:35:10 +0800

    优化构建脚本

--- .build_cid/build.sh
+++ .build_cid/build.sh
@@ -2,8 +2,8 @@
 # The build script
 # 2021-05-09 20:02:15 CST
 
-PROJECT_PATH=$(cd `dirname $0`/..; pwd)
-cd $PROJECT_PATH
-version=$(cat version |tr -d ' \t\r')
+PROJECT_PATH=$(cd "$(dirname "$0")/.." || exit; pwd)
+cd "$PROJECT_PATH" || exit
+version=$(cat < version |tr -d ' \t\r')
 images=harbor.hub.com/library/ledger-service:"$version"
 docker build --build-arg version="$version" -t "$images" . -f .build_cid/Dockerfile
