1826581c094880b6c71da2837acc07eb4b9244a4
Lark <lark.email@lark.com>
2025-08-21 13:31:57 +0800

    表结构设计

--- src/database/sql/script/v1.0.0_create_table_up.sql
+++ src/database/sql/script/v1.0.0_create_table_up.sql
@@ -1,19 +1,34 @@
+-- ----------------------------
+-- Table structure for ledger_category
+-- ----------------------------
 CREATE TABLE IF NOT EXISTS `ledger_category`  (
   `kid` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '资金的支出或收入类型',
-  `pid` int(11) NOT NULL DEFAULT 1 COMMENT '类别，1：代表大类，不可变，子类的值为该大类的id',
-  `type` int(11) NOT NULL DEFAULT 1,
-  `sort` int(11) NOT NULL,
+  `name` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '大类名称',
   `create_time` bigint(19) NULL DEFAULT 1000000000000,
   `update_time` bigint(19) NULL DEFAULT 1000000000000,
   PRIMARY KEY (`kid`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1001 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '资金来往分类表' ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 1001 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '收支大类表' ROW_FORMAT = DYNAMIC;
 
+-- ----------------------------
+-- Table structure for ledger_subcategory
+-- ----------------------------
+CREATE TABLE IF NOT EXISTS `ledger_subcategory`  (
+  `kid` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '子类名称',
+  `category_id` int(11) NOT NULL DEFAULT 1 COMMENT '外键，关联大类表的 id',
+  `create_time` bigint(19) NULL DEFAULT 1000000000000,
+  `update_time` bigint(19) NULL DEFAULT 1000000000000,
+  PRIMARY KEY (`kid`) USING BTREE,
+  INDEX `category_id`(`category_id`) USING BTREE,
+  CONSTRAINT `ledger_subcategory_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `ledger_category` (`kid`) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE = InnoDB AUTO_INCREMENT = 1001 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '收支子类表' ROW_FORMAT = DYNAMIC;
 
---
+-- ----------------------------
+-- Table structure for ledger_detail
+-- ----------------------------
 CREATE TABLE IF NOT EXISTS `ledger_detail`  (
   `kid` int(11) NOT NULL AUTO_INCREMENT,
-  `category_id` int(11) NOT NULL COMMENT 'ledger_money_type 的id',
+  `subcategory_id` int(11) NOT NULL COMMENT '关联到子类(间接关联大类)',
   `occ_time` int(11) NOT NULL COMMENT '支出或收入发生的年，（occurrence year）',
   `amount` decimal(19, 4) NOT NULL COMMENT '金额',
   `total` int(11) NOT NULL DEFAULT 1 COMMENT '是否计入本月收支，1：计入；0：不计入',
@@ -23,6 +38,6 @@ CREATE TABLE IF NOT EXISTS `ledger_detail`  (
   `create_time` bigint(19) NULL DEFAULT 1000000000000,
   `update_time` bigint(19) NULL DEFAULT 1000000000000,
   PRIMARY KEY (`kid`) USING BTREE,
-  INDEX `category_id`(`category_id`) USING BTREE,
-  CONSTRAINT `ledger_detail_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `ledger_category` (`kid`) ON DELETE RESTRICT ON UPDATE CASCADE
+  INDEX `category_id`(`subcategory_id`) USING BTREE,
+  CONSTRAINT `ledger_detail_ibfk_1` FOREIGN KEY (`subcategory_id`) REFERENCES `ledger_subcategory` (`kid`) ON DELETE RESTRICT ON UPDATE CASCADE
 ) ENGINE = InnoDB AUTO_INCREMENT = 1001 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '收支明细' ROW_FORMAT = DYNAMIC;
